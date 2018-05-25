3e521ddfb643836a5ad1051b66c57fc9c6768ae4
Madhu Rajanna <mrajanna@redhat.com>
2018-05-25 11:29:15 +0530

    implement device executor functionality in heketi to support  glusterd2
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- executors/glusterd/device.go
+++ executors/glusterd/device.go
@@ -10,6 +10,9 @@
 package glusterd
 
 import (
+	"encoding/json"
+	"errors"
+
 	"github.com/heketi/heketi/executors"
 )
 
@@ -37,30 +40,17 @@ const (
 // https://access.redhat.com/documentation/en-US/Red_Hat_Storage/3.1/html/Administration_Guide/Brick_Configuration.html
 //
 
-func (g *GlusterdExecutor) DeviceSetup(host, device, vgid string, destroy bool) (d *executors.DeviceInfo, e error) {
+// deviceInfo represents structure in which devices are to be store in Peer Metadata
+type deviceInfo struct {
+	Name          string `json:"name"`
+	State         string `json:"state"`
+	VgName        string `json:"vg-name"`
+	AvailableSize uint64 `json:"available-size"`
+	ExtentSize    uint64 `json:"extent-size"`
+	Used          bool   `json:"used"`
+}
 
-	// Setup commands
-	// commands := []string{}
-
-	// if destroy {
-	// 	logger.Info("Data on device %v (host %v) will be destroyed", device, host)
-	// 	commands = append(commands, fmt.Sprintf("wipefs --all %v", device))
-	// }
-	// commands = append(commands, fmt.Sprintf("pvcreate --metadatasize=128M --dataalignment=256K '%v'", device))
-	// commands = append(commands, fmt.Sprintf("vgcreate --autobackup=%v %v %v", utils.BoolToYN(s.BackupLVM), utils.VgIdToName(vgid), device))
-
-	// // Execute command
-	// _, err := s.RemoteExecutor.RemoteCommandExecute(host, commands, 5)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// // Create a cleanup function if anything fails
-	// defer func() {
-	// 	if e != nil {
-	// 		s.DeviceTeardown(host, device, vgid)
-	// 	}
-	// }()
+func (g *GlusterdExecutor) DeviceSetup(host, device, vgid string, destroy bool) (*executors.DeviceInfo, error) {
 	g.createClient(host)
 	peerid := ""
 	peerlist, err := g.Client.Peers()
@@ -75,66 +65,79 @@ func (g *GlusterdExecutor) DeviceSetup(host, device, vgid string, destroy bool) 
 			}
 		}
 	}
-	deviceinfo, err := g.Client.DeviceAdd(peerid, device)
+	//TODO implement device delete in failed scenario?
+	dev, err := g.Client.DeviceAdd(peerid, device)
+	if err != nil {
+		logger.Err(err)
+		return nil, err
+	}
+	var devices []deviceInfo
+	var info = new(executors.DeviceInfo)
+	if _, exist := dev.Metadata["_devices"]; exist {
 
-	_ = deviceinfo
-	_ = err
-	return nil, executors.NotSupportedError
+		err = json.Unmarshal([]byte(dev.Metadata["_devices"]), &devices)
+		if err != nil {
+			logger.Err(err)
+			return nil, err
+		}
+		for _, d := range devices {
+			if device == d.Name {
+				info.ExtentSize = d.ExtentSize
+				info.Size = d.AvailableSize
+			}
+		}
+	}
+	if info == nil {
+		logger.LogError("failed to fetch device details")
+		return nil, errors.New("failed to fetch device details")
+	}
+	return info, nil
 }
 
 func (g *GlusterdExecutor) GetDeviceInfo(host, device, vgid string) (d *executors.DeviceInfo, e error) {
-	// Vg info
+	//TODO need to replace this function by listing device
 	d = &executors.DeviceInfo{}
-	//err := g.getVgSizeFromNode(d, host, device, vgid)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	//return d, nil
-	return nil, executors.NotSupportedError
+
+	g.createClient(host)
+
+	peerlist, err := g.Client.Peers()
+	if err != nil {
+		logger.Err(err)
+		return nil, err
+	}
+	var devices []deviceInfo
+	var info = new(executors.DeviceInfo)
+	for _, peer := range peerlist {
+		for _, addr := range peer.PeerAddresses {
+			if addr == host+g.Config.ClientPORT {
+				if _, exist := peer.Metadata["_devices"]; exist {
+
+					err = json.Unmarshal([]byte(peer.Metadata["_devices"]), &devices)
+					if err != nil {
+						logger.Err(err)
+						return nil, err
+					}
+					for _, d := range devices {
+						if device == d.Name {
+							info.ExtentSize = d.ExtentSize
+							info.Size = d.AvailableSize
+							break
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+	if info == nil {
+		logger.LogError("failed to fetch device details")
+		return nil, errors.New("failed to fetch device details")
+	}
+	return info, nil
 }
 
 func (g *GlusterdExecutor) DeviceTeardown(host, device, vgid string) error {
+	//TODO need to implement this api
 	return executors.NotSupportedError
 }
-
-// func (g *GlusterdExecutor) getVgSizeFromNode(
-// 	d *executors.DeviceInfo,
-// 	host, device, vgid string) error {
-
-// 	// Setup command
-// 	commands := []string{
-// 		fmt.Sprintf("vgdisplay -c %v", utils.VgIdToName(vgid)),
-// 	}
-
-// 	// Execute command
-// 	b, err := s.RemoteExecutor.RemoteCommandExecute(host, commands, 5)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// Example:
-// 	// sampleVg:r/w:772:-1:0:0:0:-1:0:4:4:2097135616:4096:511996:0:511996:rJ0bIG-3XNc-NoS0-fkKm-batK-dFyX-xbxHym
-// 	vginfo := strings.Split(b[0], ":")
-
-// 	// See vgdisplay manpage
-// 	if len(vginfo) < 17 {
-// 		return errors.New("vgdisplay returned an invalid string")
-// 	}
-
-// 	extent_size, err :=
-// 		strconv.ParseUint(vginfo[VGDISPLAY_PHYSICAL_EXTENT_SIZE], 10, 64)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	free_extents, err :=
-// 		strconv.ParseUint(vginfo[VGDISPLAY_FREE_NUMBER_EXTENTS], 10, 64)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	d.Size = free_extents * extent_size
-// 	d.ExtentSize = extent_size
-// 	logger.Debug("Size of %v in %v is %v", device, host, d.Size)
-// 	return nil
-// }
